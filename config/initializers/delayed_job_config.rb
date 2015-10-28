5d7287eea367151177c84f68f75efa24b643877e
Michael Blatter <mblatter@spartzinc.com>
2015-10-28 10:51:04 -0500

    Updates

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -1,9 +1,4 @@
 Delayed::Worker.destroy_failed_jobs = false
 Delayed::Worker.max_attempts = 1
-
-unless File.exists?(File.join(Rails.root, 'log', 'delayed_job.log'))
-  FileUtils.touch(File.join(Rails.root, 'log', 'delayed_job.log'))
-end
-
 Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
 Delayed::Worker.sleep_delay = 5
